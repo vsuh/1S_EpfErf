06a0749d50067d766b6ba577c147656253a01c13
vsuh <cyx@bk.ru>
2015-11-04 11:14:39 +0300

    убраны ненужные файлы

--- old/01_РаспаковкаФайловОбработок.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-md in out ok >nul 2>&1
-cls&del log.*
-
-echo %date% %time% ^*^*^* распаковка файлов отчетов
-for %%I in (in\*) do ( 
-		echo %%~nI >>log.log 
-		v8\v8unpack.exe -parse "%%I" "out\%%~nxI" >>log.log 
-	)
